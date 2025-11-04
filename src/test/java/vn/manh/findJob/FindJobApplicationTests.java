72b5e5b9dfeb6ee0cfbb1260b53c153e0f41aeca
NguyenVanManh0802 <tonymanh12345@gmail.com>
2025-11-04 21:17:44 +0700

    start project

---
+++ src/test/java/vn/manh/findJob/FindJobApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.manh.findJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
